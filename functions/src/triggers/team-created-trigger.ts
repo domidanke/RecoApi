bf732cd9358b69e399fa814d2b01644f3fe19698
Hardiansen <fabi.stange@gmx.de>
2021-11-21 17:02:49 +0100

    new Team trigger rewrite

--- functions/src/triggers/team-created-trigger.ts
+++ functions/src/triggers/team-created-trigger.ts
@@ -4,17 +4,20 @@ import {QueryDocumentSnapshot} from 'firebase-functions/v1/firestore';
 import {Team} from '../models/team/team';
 
 admin.initializeApp();
-const db = admin.firestore();
 // TODO write TEST
 const onCreateTeam = functions.firestore
   .document('teams/{teamId}')
   .onCreate((snap: QueryDocumentSnapshot) => {
     const newTeam = snap.data() as Team;
     const arrayUnion = admin.firestore.FieldValue.arrayUnion;
-    db.doc(`users/${newTeam.creatorId}`).update({
-      'recentTeamId': newTeam.id,
-      'teamIds': arrayUnion(newTeam.id),
-    });
+    admin
+      .firestore()
+      .collection('users')
+      .doc(newTeam.creatorId)
+      .update({
+        'recentTeamId': newTeam.id,
+        'teamIds': arrayUnion(newTeam.id),
+      });
   });
 
 module.exports = onCreateTeam;
