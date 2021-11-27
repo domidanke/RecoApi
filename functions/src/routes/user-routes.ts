326c58d9a19b01ad00e74f0f7d4a6570d63062f0
Hardiansen <fabi.stange@gmx.de>
2021-11-27 20:13:20 +0100

    linter rule

--- functions/src/routes/user-routes.ts
+++ functions/src/routes/user-routes.ts
@@ -136,7 +136,9 @@ router.post('/:injuryId/:stageId', async (req, res) => {
           res.send(404).send('Stage does not exist');
         } else {
           const stage = stageSnap.data() as InjuryStage;
-          const exerciseIds = stage.exercises.map((x) => x.id);
+          const exerciseIds = stage.exercises.map((x) => {
+            return x.id;
+          });
           await admin
             .firestore()
             .collection('exercises')
